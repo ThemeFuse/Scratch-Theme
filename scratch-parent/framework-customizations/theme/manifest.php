f7c08ab349635ef40dd8b1c88391f4d5bf22b4c4
moldcraft <moldcraft@email.com>
2014-12-03 21:07:23 +0200

    updated the compatible extensions list

--- scratch-parent/framework-customizations/theme/manifest.php
+++ scratch-parent/framework-customizations/theme/manifest.php
@@ -7,6 +7,14 @@ $manifest = array();
 $manifest['id'] = 'scratch';
 
 $manifest['supported_extensions'] = array(
+	'page-builder' => array(),
+	'slider' => array(),
+	'styling' => array(),
+	'breadcrumbs' => array(),
+	'events' => array(),
+	'feedback' => array(),
+	'learning' => array(),
+	'megamenu' => array(),
+	'portfolio' => array(),
 	'sidebars' => array(),
-	'hello' => array(),
 );
