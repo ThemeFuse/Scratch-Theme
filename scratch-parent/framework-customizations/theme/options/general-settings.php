30e662d1ce6e733dbc1d6003eef175b428674e40
Valeriu <valeriuzdrobau@gmail.com>
2014-12-25 11:53:15 +0200

    minor fix

--- scratch-parent/framework-customizations/theme/options/general-settings.php
+++ scratch-parent/framework-customizations/theme/options/general-settings.php
@@ -21,36 +21,7 @@ $options = array(
 						'label' => __( 'Favicon', 'unyson' ),
 						'desc'  => __( 'Upload a favicon image', 'unyson' ),
 						'type'  => 'upload'
-					),
-					'test_slider10' => array(
-						'label' => __( 'Slider 1', 'unyson' ),
-						'desc'  => __( 'fgi teg tg tg trt', 'unyson' ),
-						'type'  => 'slider',
-						'value' => array(
-							'from'  => 100,
-							'to'    => 500
-						),
-						'properties'   => array(
-							'min'   => 0,
-							'max'   => 10000,
-							'step'  => 500,
-							//'grid'  => true,
-							//'grid_snap' => true,
-						),
-					),
-					'test_slider11' => array(
-						'label' => __( 'Slider 2', 'unyson' ),
-						'desc'  => __( 'fgi teg tg tg trt', 'unyson' ),
-						'type'  => 'slider',
-						'value' => 4000,
-						'properties'   => array(
-							'min'   => 0,
-							'max'   => 10000,
-							'step'  => 1000,
-							'grid'  => true,
-							'grid_snap' => true
-						),
-					),
+					)
 				)
 			),
 		)
