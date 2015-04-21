e1bc1d62bcb4229160eff4e2ba2e9d9cceac38a8
Alexei Gogu <llemurya@gmail.com>
2015-04-21 13:34:21 +0300

    owl-carousel small-fix

--- scratch-parent/framework-customizations/extensions/media/extensions/slider/extensions/owl-carousel/views/owl-carousel.php
+++ scratch-parent/framework-customizations/extensions/media/extensions/slider/extensions/owl-carousel/views/owl-carousel.php
@@ -16,7 +16,7 @@
 	<div class="owl-carousel owl-theme">
 		<?php foreach ($data['slides'] as $slide): ?>
 			<?php if ($slide['multimedia_type'] === 'video' && false !== wp_oembed_get($slide['src'])) : ?>
-				<div class="item-video" style="height:<?php echo $dimensions['height'];?>px; width:<?php echo $dimensions['width'];?>px;"><a class="owl-video" href="<?php echo $slide['src'] ?>"></a></div>
+				<div class="item-video" style="height:<?php echo $dimensions['height'];?>px;"><a class="owl-video" href="<?php echo $slide['src'] ?>"></a></div>
 			<?php endif; ?>
 
 		<?php endforeach; ?>
